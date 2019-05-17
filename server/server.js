b9388e2a05b2ab6eb16ce60390b439a90b24bdc8
Emerson Demetrio <emer.demetrio@gmail.com>
2019-05-16 23:13:54 -0300

    Server - Heroku Setup

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const process = require('process');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
@@ -18,6 +19,26 @@ app.use(bodyParser.urlencoded({
 
 app.use('/api/movies', moviesRouter);
 app.use('/api/configuration', configurationRouter);
+
+const allowedExt = [
+	'.js',
+	'.ico',
+	'.css',
+	'.png',
+	'.jpg',
+	'.woff2',
+	'.woff',
+	'.ttf',
+	'.svg',
+];
+
+app.get('*', (req, res) => {
+	if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+		res.sendFile(path.resolve(`dist/upcoming-movies/${req.url}`));
+	} else {
+		res.sendFile(path.resolve('dist/upcoming-movies/index.html'));
+	}
+});
 
 app.listen(port, async () => {
 	console.log('Server running on port 3000');
